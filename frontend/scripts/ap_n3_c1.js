dc569b383f2972ca5e1b0645b79f99eff5e257b4
baileytorch <baileytorch@hotmail.com>
2026-06-09 16:23:25 -0400

    Actualizando formulario y validaciones del lado del cliente

--- frontend/scripts/ap_n3_c1.js
+++ frontend/scripts/ap_n3_c1.js
@@ -8,43 +8,41 @@ function validarFormulario() {
     let fechaNacimiento = document.getElementById('inputFechaNac');
     let genero = document.querySelector('input[name="radioGenero"]:checked');
     let pais = document.getElementById('selectPais');
-    let formularioValido = false;
+    let formularioValido = true;
 
-    if (formularioValido == false) {
-        if (!validarCampo(nombre)) {
-            formularioValido = false;
-        } else { formularioValido = true }
-
-        if (!validarCampo(email) || !validarEmail(email.value)) {
-            formularioValido = false;
-        } else { formularioValido = true }
-
-        if (!validarCampo(rut) || !validarRut(rut)) {
-            formularioValido = false;
-        } else { formularioValido = true }
-
-        if (!validarCampo(telefono)) {
-            formularioValido = false
-        } else { formularioValido = true }
-
-        if (!validarCampo(contrasena) || !validarContrasena(contrasena.value)) {
-            formularioValido = false
-        } else { formularioValido = true }
-
-        if (!validarCampo(repContrasena) || repContrasena.value != contrasena.value) {
-            formularioValido = false
-        } else { formularioValido = true }
-
-        if (!validarCampo(fechaNacimiento)) {
-            formularioValido = false
-        } else { formularioValido = true }
-
-        if (!validarCampo(pais)) {
-            formularioValido = false
-        } else { formularioValido = true }
+    if (!validarCampo(nombre)) {
+        formularioValido = false;
     }
 
-    if (formularioValido == true) {
+    if (!validarEmail(email)) {
+        formularioValido = false;
+    }
+
+    if (!validarRut(rut)) {
+        formularioValido = false;
+    }
+
+    if (!validarCampo(telefono)) {
+        formularioValido = false
+    }
+
+    if (!validarContrasena(contrasena)) {
+        formularioValido = false
+    }
+
+    if (!validarRepetirContrasena(repContrasena, contrasena)) {
+        formularioValido = false
+    }
+
+    if (!validarCampo(fechaNacimiento)) {
+        formularioValido = false
+    }
+
+    if (!validarCampo(pais)) {
+        formularioValido = false
+    }
+
+    if (formularioValido) {
         alert('Datos ingresados correctamente, enviado al servidor...')
     }
 }
@@ -60,38 +58,75 @@ function validarCampo(campo) {
     }
 }
 
-function validarEmail(email) {
-    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regexEmail.test(email);
+function validarEmail(campo) {
+    if (validarCampo(campo)) {
+        const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        if (!regexEmail.test(campo.value)) {
+            campo.classList.add('is-invalid', 'alerta');
+            return false
+        } else {
+            campo.classList.remove('is-invalid', 'alerta');
+            campo.classList.add('is-valid');
+            return true
+        }
+    }
 };
 
-function validarContrasena(contrasena) {
-    const regexContrasena = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
-    return regexContrasena.test(contrasena);
+function validarContrasena(campo) {
+    if (validarCampo(campo)) {
+        const regexContrasena = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+        if (regexContrasena.test(campo.value)) {
+            campo.classList.remove('is-invalid', 'alerta');
+            campo.classList.add('is-valid');
+            return true
+        } else {
+            campo.classList.add('is-invalid', 'alerta');
+            return false
+        }
+    }
 }
 
-function validarRut(rut) {
-    var valor = rut.value.replace('.', '');
-    valor = valor.replace('-', '');
-
-    cuerpo = valor.slice(0, -1);
-    dv = valor.slice(-1).toUpperCase();
-    rut.value = cuerpo + '-' + dv
-
-    if (cuerpo.length < 7) { return false; }
-    suma = 0;
-    multiplo = 2;
-
-    for (i = 1; i <= cuerpo.length; i++) {
-        index = multiplo * valor.charAt(cuerpo.length - i);
-        suma = suma + index;
-        if (multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+function validarRepetirContrasena(campo, campo2) {
+    if (validarCampo(campo)) {
+        if (campo.value === campo2.value) {
+            campo.classList.remove('is-invalid', 'alerta');
+            campo.classList.add('is-valid');
+            return true
+        } else { campo.classList.add('is-invalid', 'alerta'); return false }
     }
+}
 
-    dvEsperado = 11 - (suma % 11);
+function validarRut(campo) {
+    if (validarCampo(campo)) {
+        var valor = campo.value.replace('.', '');
+        valor = valor.replace('-', '');
 
-    dv = (dv == 'K') ? 10 : dv;
-    dv = (dv == 0) ? 11 : dv;
+        cuerpo = valor.slice(0, -1);
+        dv = valor.slice(-1).toUpperCase();
+        campo.value = cuerpo + '-' + dv
 
-    return dvEsperado == dv
+        if (cuerpo.length < 7) { return false; }
+        suma = 0;
+        multiplo = 2;
+
+        for (i = 1; i <= cuerpo.length; i++) {
+            index = multiplo * valor.charAt(cuerpo.length - i);
+            suma = suma + index;
+            if (multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+        }
+
+        dvEsperado = 11 - (suma % 11);
+
+        dv = (dv == 'K') ? 10 : dv;
+        dv = (dv == 0) ? 11 : dv;
+
+        if (dvEsperado == dv) {
+            campo.classList.remove('is-invalid', 'alerta');
+            campo.classList.add('is-valid');
+            return true
+        } else {
+            campo.classList.add('is-invalid', 'alerta');
+            return false
+        }
+    }
 }
